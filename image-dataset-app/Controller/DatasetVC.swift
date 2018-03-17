afd5d8def013cd05fa3bbca5c3dd1016f2a3bd78
Krzysztof Langner <klangner@gmail.com>
2018-03-17 12:06:44 +0100

    Popup mechanic working

--- image-dataset-app/Controller/DatasetVC.swift
+++ image-dataset-app/Controller/DatasetVC.swift
@@ -15,18 +15,18 @@ class DatasetVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // Switch to the view which will take new picture
     @IBAction func takePictureTapped(_ sender: Any) {
         print("Take picture")
     }
     
     @IBAction func selectDatasetTapped(_ sender: Any) {
-        print("Select dataset")
+        if let popupVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "datasetsPopup") as? DatasetsPopupVC {
+            self.addChildViewController(popupVC)
+            popupVC.view.frame = view.frame
+            view.addSubview(popupVC.view)
+            popupVC.didMove(toParentViewController: self)
+        }
     }
     
 }
---
+++ image-dataset-app/Controller/DatasetsPopupVC.swift
@@ -0,0 +1,26 @@
+//
+//  SelectDatasetVCViewController.swift
+//  image-dataset-app
+//
+//  Created by Krzysztof Langner on 17/03/2018.
+//  Copyright © 2018 Krzysztof Langner. All rights reserved.
+//
+
+import UIKit
+
+class DatasetsPopupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        closePopup()
+    }
+    
+    func closePopup() {
+        view.removeFromSuperview()
+    }
+}
